4ac89b300c22a231e70ba4e5f05b416121fdff5d
Admin <Admin@YPHP>
2026-09-11 21:44:14 -0700

    fix spaproxy - can not be use with reverse proxy env.

--- webdownload.client/src/proxy.conf.js
+++ webdownload.client/src/proxy.conf.js
@@ -1,7 +1,7 @@
 const { env } = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://127.0.0.1:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7115';
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://127.0.0.1:5001';
 
 const PROXY_CONFIG = [
   {
